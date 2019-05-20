41c6876b18d1cfe8954b4a0fc5bf67476e417d68
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-20 13:55:04 -0700

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClassComponent } from './class/class.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/class/class.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-class',
+  templateUrl: './class.component.html',
+  styleUrls: ['./class.component.css']
+})
+export class ClassComponent implements OnInit {
+  myclass = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
